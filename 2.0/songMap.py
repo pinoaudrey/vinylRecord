6e47124676ef985b3e71ba1e97752e55a186c16f
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-06-03 20:36:09 -0600

    Create songMap.py

---
+++ 2.0/songMap.py
@@ -0,0 +1,113 @@
+songMap = {
+    964424835454: {
+        "name": "Billie Eilish - Happier than ever",
+        "sid": 'spotify:album:0JGOiO34nwfUdDrD612dOp',
+        "type": "album"
+        },
+    700780273704: {
+        "name": "Yebba - Love Came Down",
+        "sid": 'spotify:track:1B2amGFcCiluKPYy97epQ2',
+        "type": "song"
+        },
+    700629344288: {
+        "name": "Guns n' Roses - Use Your Illusion II",
+        "sid": 'spotify:album:00eiw4KOJZ7eC3NBEpmH4C',
+        "type": "album"
+        },
+    82204450964: {
+        "name": "Bruno Mars - Doo-Wops & Hooligans",
+        "sid": 'spotify:album:1uyf3l2d4XYwiEqAb7t7fX',
+        "type": "album"
+        },
+    150957547683: {
+        "name": 'Childish Gambino - "Awaken, My Love!"',
+        "sid": 'spotify:album:4Carzsnpd6yvuHZ49I0oz8',
+        "type": "album"
+        },
+    82355576985: {
+        "name": "Eagles - Hotel California (2013 Remastered)",
+        "sid": 'spotify:album:2widuo17g5CEC66IbzveRu',
+        "type": "album"
+        },
+    82187870358: {
+        "name": "BØRNS - Blue Madonna",
+        "sid": 'spotify:album:0mzh0tCNFhxE2mSWxB4ufh',
+        "type": "album"
+        },
+    631624982580: {
+        "name": "Quen - Bohemain Rhapsody",
+        "sid": 'spotify:album:3BHe7LbW5yRjyqXNJ3A6mW',
+        "type": "album"
+        },
+    288228991217: {
+        "name": 'Amy Winehouse - Back To Black',
+        "sid": 'spotify:album:097eYvf9NKjFnv4xA9s2oV',
+        "type": "album"
+        },
+    288531046596: {
+        "name": "Coldplay - Parachutes",
+        "sid": 'spotify:album:6ZG5lRT77aJ3btmArcykra',
+        "type": "album"
+        },
+    288900210859: {
+        "name": "An Evening With Herbie Hancock & Chick Corea In Concert (Live)",
+        "sid": 'spotify:album:2xGIvFXBvdU4HHI5wIzWzx',
+        "type": "album"
+        },
+    150858784958: {
+        "name": "J. Cole - 2014 Forest Hills Drive ",
+        "sid": 'spotify:album:0UMMIkurRUmkruZ3KGBLtG',
+        "type": "album"
+        },
+    975777783888: {
+        "name": 'Billy Joel - 52nd Street',
+        "sid": 'spotify:album:1HmCO8VK98AU6EXPOjGYyI',
+        "type": "album"
+        },
+    357067677941: {
+        "name": "Louis Armstrong - The Platinum Collection (36 All Time Classics)",
+        "sid": 'spotify:playlist:5OVxIIISY8iYSIBrRcEghU',
+        "type": "playlist"
+        },
+    82290499774: {
+        "name": "Frank Sinatra -  Songs For The Young At Heart",
+        "sid": 'spotify:playlist:2jRoRRvxsSMTOYNrteHYFN',
+        "type": "playlist"
+        },
+    769736990722: {
+        "name": "Guns n' Roses - Use Your Illusion I",
+        "sid": 'spotify:album:0CxPbTRARqKUYighiEY9Sz',
+        "type": "album"
+        },
+    770055823486: {
+        "name": 'Led Zepplin IV (Deluxe Edition)',
+        "sid": 'spotify:album:44Ig8dzqOkvkGDzaUof9lK',
+        "type": "album"
+        },
+    564082008158: {
+        "name": "Tom Misch & Yussef Dayes - What Kinda Music",
+        "sid": 'spotify:album:6iOCv7oGL5sGi2aVnRz2BI',
+        "type": "album"
+        },
+    702494040228: {
+        "name": "Santana - Abraxas",
+        "sid": 'spotify:album:1CHUXwuge9A7L2KiA3vnR6',
+        "type": "album"
+        },
+    221424214071: {
+        "name": "Freddie Mercury & Montserrat - Barcelona",
+        "sid": 'spotify:album:1SFN6nc1HofjuhDvhjLvMC',
+        "type": "album"
+        },
+    84035657737: {
+        "name": "Tyler The Creator - Flower Boy",
+        "sid": 'spotify:album:2nkto6YNI4rUYTLqEwWJ3o',
+        "type": "album"
+        },
+    772703346830: {
+        "name": "Mashrou' Leila - Ibn El Leil (Deluxe Edition)",
+        "sid": 'spotify:album:5Q7d7KTIIYUnemuqHU2zBD',
+        "type": "album"
+        },
+
+} 
